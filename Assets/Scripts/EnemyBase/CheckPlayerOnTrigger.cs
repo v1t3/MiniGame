53914bda49fb5913cf4acda427dd82f0cfa92ea3
Vitali <37334443+v1t3@users.noreply.github.com>
2021-09-19 22:10:41 +0300

    Updated hen

---
+++ Assets/Scripts/EnemyBase/CheckPlayerOnTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using PlayerBase;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace EnemyBase
+{
+    public class CheckPlayerOnTrigger : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent onPlayerVisible;
+        [SerializeField] private UnityEvent onPlayerInvisible;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (
+                other.attachedRigidbody &&
+                other.attachedRigidbody.GetComponent<PlayerMove>()
+            )
+            {
+                onPlayerVisible.Invoke();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (
+                other.attachedRigidbody &&
+                other.attachedRigidbody.GetComponent<PlayerMove>()
+            )
+            {
+                onPlayerInvisible.Invoke();
+            }
+        }
+    }
+}
--- Assets/Scripts/Hen.cs
+++ Assets/Scripts/Hen.cs
@@ -13,6 +13,8 @@ public class Hen : MonoBehaviour
     [SerializeField] private float speed = 3;
     [SerializeField] private float timeToReachSpeed = 5;
 
+    private bool _isPlayerVisible;
+
     private void Start()
     {
         _playerTransform = FindObjectOfType<PlayerMove>().transform;
@@ -20,9 +22,17 @@ public class Hen : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 toPlayer = (_playerTransform.position - transform.position).normalized;
-        Vector3 force = rb.mass * (toPlayer * speed - rb.velocity) / timeToReachSpeed;
-        
-        rb.AddForce(force);
+        if (_isPlayerVisible && timeToReachSpeed > 0)
+        {
+            Vector3 toPlayer = (_playerTransform.position - transform.position).normalized;
+            Vector3 force = rb.mass * (toPlayer * speed - rb.velocity) / timeToReachSpeed;
+
+            rb.AddForce(force);
+        }
+    }
+
+    public void SetPlayerVisible(bool value)
+    {
+        _isPlayerVisible = value;
     }
 }
