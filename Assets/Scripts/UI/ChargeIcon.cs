1f2641312e32794b1c2717713939c564cb832b0c
Vitali <37334443+v1t3@users.noreply.github.com>
2021-10-06 20:40:35 +0300

    Добавлено аудиоружжо

---
+++ Assets/Scripts/UI/ChargeIcon.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.VFX;
+
+namespace UI
+{
+    public class ChargeIcon : MonoBehaviour
+    {
+        [SerializeField] private Image background;
+        [SerializeField] private Image foreground;
+        [SerializeField] private Text text;
+
+        public void StartCharge()
+        {
+            background.color = new Color(1, 1, 1, 0.2f);
+            foreground.enabled = true;
+            text.enabled = true;
+        }
+
+        public void StopCharge()
+        {
+            background.color = new Color(1, 1, 1, 1);
+            foreground.enabled = false;
+            text.enabled = false;
+        }
+
+        public void SetChargeValue(float currentCharge, float maxCharge)
+        {
+            foreground.fillAmount = currentCharge / maxCharge;
+            text.text = Mathf.Ceil(maxCharge - currentCharge).ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponBase/JumpGun.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UI;
+using UnityEngine;
+
+namespace WeaponBase
+{
+    public class JumpGun : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody playerRb;
+        [SerializeField] private float speed;
+        [SerializeField] private Transform spawn;
+        [SerializeField] private Gun pistol;
+
+        [SerializeField] private ChargeIcon chargeIcon;
+
+        public float maxCharge;
+
+        private float _currentCharge;
+        private bool _isCharged;
+
+        private void Update()
+        {
+            if (_currentCharge < maxCharge)
+            {
+                _currentCharge += Time.deltaTime;
+                _isCharged = false;
+                chargeIcon.SetChargeValue(_currentCharge, maxCharge);
+            }
+            else
+            {
+                _isCharged = true;
+                chargeIcon.StopCharge();
+            }
+
+            if (_isCharged && Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
+        }
+
+        private void Shoot()
+        {
+            pistol.Shot();
+            playerRb.AddForce(-spawn.forward * speed, ForceMode.VelocityChange);
+            _currentCharge = 0;
+            _isCharged = false;
+            chargeIcon.StartCharge();
+        }
+    }
+}
